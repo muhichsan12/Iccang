c56bb1927de2369bea7619ce5daa603f36680580
muhichsan12 <muhalqialif12@gmail.com>
2023-05-09 12:49:54 +0800

    first commit

---
+++ controllers/KrsMahasiswaController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controllers;
+
+class KrsMahasiswaController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+
+    public function actionUpdate()
+    {
+        return $this->render('Update');
+    }
+
+    public function actionDelete()
+    {
+        return $this->render('delete');
+    }
+    
+}
---
+++ views/mahasiswa/profil.php
@@ -0,0 +1,3 @@
+Nama: Muh.Ichsan Pratama Putra <br>
+Nim: 60200121004 <br>
+Kelas: A <br>
